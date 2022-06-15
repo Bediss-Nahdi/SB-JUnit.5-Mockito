12c61b07ce6961d57822243b7f198cfe431128ac
Nahdi <nahdibediss@yahoo.fr>
2022-06-15 14:59:51 +0200

    Mise en place du projets avec les dependences

---
+++ junit-api-rest-application/src/main/java/fr/junit/JunitApiRestApplication.java
@@ -0,0 +1,13 @@
+package fr.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitApiRestApplication.class, args);
+	}
+
+}
---
+++ junit-api-rest-application/src/test/java/fr/junit/JunitApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
